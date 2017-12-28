e9bd557b7cdbb63fbd7fd300adc5ea8f0f57f5ab
James Clarke <james@clarkezone.net>
2017-12-27 21:42:12 -0800

    add gcc compile step

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use std::ops::Index;
 use ast::ast::parse;
 use lexer::lexer::lex;
 use std::path::Path;
+use std::path::PathBuf;
 use std::fs;
 use codegen::codegen::CodeGenState;
+use std::process::Command;
 
 use std::env;
 
@@ -23,18 +25,7 @@ fn main() {
 
     let thepath = Path::new(args.index(1));
 
-    //let output_file = Path::new("output.S");
     let output_path = thepath.with_extension("S");
-
-    // output_path = match thepath.parent() {
-    //     Some(p) => {
-    //         //p.join(output_file);
-    //         output_file.with_file_name(file_name)
-    //     }
-    //     None => {
-    //         output_file.as_ref();
-    //     }
-    // };
 
     let token_list = lex(thepath);
 
@@ -45,5 +36,48 @@ fn main() {
     });
 
     let mut code_gen = CodeGenState::new(result);
-    code_gen.generate_code_to_file(output_path);
+    code_gen.generate_code_to_file(&output_path);
+
+    gcc_compile(&output_path);
+}
+
+fn gcc_compile(file_path: &PathBuf) {
+    let mut naked = file_path.clone();
+    naked.set_extension("");
+    let clean_str = match naked.to_str() {
+        Some(s) => s,
+        None => {
+            panic!("broken");
+        }
+    };
+
+    let compile = format!("gcc {}.S -o {}", clean_str, clean_str);
+    if cfg!(target_os = "windows") {
+        println!("<WARNING> Running on windows.. skipping gcc step");
+        println!("{}", compile);
+        return;
+    }
+
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", "gcc file"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(clean_str)
+            .output()
+            .expect("failed to execute process")
+    };
+
+    let hello = std::str::from_utf8(&output.stdout);
+    match hello {
+        Ok(s) => {
+            println!("{}", s);
+        }
+        Err(_) => {
+            panic!("Can't read string");
+        }
+    }
 }
